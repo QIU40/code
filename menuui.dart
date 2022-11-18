def2cb0118dee0b8093cceaff573ec491ff5a54c
QIU40 <106186013+QIU40@users.noreply.github.com>
2022-11-19 02:11:54 +0800

    Add files via upload

---
+++ menuui.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class MenuPage extends StatefulWidget {
+  const MenuPage({Key? key}) : super(key: key);
+
+  @override
+  State<MenuPage> createState() => _MenuPage();
+}
+
+class _MenuPage extends State<MenuPage> {
+  get editingController => TextEditingController();
+  get editingController2 => TextEditingController();
+
+  swapValues() {
+    final temp = editingController.text;
+    setState(() {
+      editingController.text = editingController2.text;
+      editingController2.text = temp;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                onChanged: (value) {},
+                controller: editingController,
+                decoration: InputDecoration(
+                    labelText: "Pickup",
+                    hintText: "Pickup Location",
+                    prefixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)))),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                onChanged: (value) {},
+                controller: editingController2,
+                decoration: InputDecoration(
+                    labelText: "Dropoff",
+                    hintText: "Dropoff Location",
+                    prefixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)))),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: swapValues,
+        tooltip: 'swap',
+        child: const Icon(Icons.swap_calls),
+      ),
+    );
+  }
+}

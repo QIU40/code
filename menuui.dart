c51ad6db62e945e79c7a2a3cec10f0f346cd934e
QIU40 <106186013+QIU40@users.noreply.github.com>
2022-11-20 07:11:58 +0800

    Update menuui.dart

--- menuui.dart
+++ menuui.dart
@@ -24,38 +24,46 @@ class _MenuPage extends State<MenuPage> {
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
-        Column(children: [
-          TextField(
-            onChanged: (value) {},
-            controller: editingController,
-            decoration: InputDecoration(
-                labelText: "Pickup",
-                hintText: "Pickup Location",
-                prefixIcon: Icon(Icons.search),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10.0)))),
-          ),
-          Align(
-            alignment: Alignment.bottomRight,
-            heightFactor: 1.0,
-            child: ElevatedButton(
-                onPressed: swapValues, child: const Text("swap")),
-          ),
-          const SizedBox(height: 15),
-          TextField(
-            onChanged: (value) {},
-            controller: editingController2,
-            decoration: InputDecoration(
-                labelText: "Dropoff",
-                hintText: "Dropoff Location",
-                prefixIcon: Icon(Icons.search),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10.0)))),
-          ),
-        ]),
+        Column(
+          children: [
+            Container(
+              margin: EdgeInsets.fromLTRB(10, 10, 80, 10), //<, ^, >, v
+              child: TextField(
+                onChanged: (value) {},
+                controller: editingController,
+                decoration: InputDecoration(
+                    labelText: "Pickup",
+                    hintText: "Pickup Location",
+                    prefixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)))),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(70, 0, 10, 0), //<, ^, >, v
+              child: Align(
+                alignment: Alignment.bottomRight,
+                heightFactor: 1.0,
+                child: ElevatedButton(
+                    onPressed: swapValues, child: const Text("swap")),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(10, 0, 80, 0),
+              child: TextField(
+                onChanged: (value) {},
+                controller: editingController2,
+                decoration: InputDecoration(
+                    labelText: "Dropoff",
+                    hintText: "Dropoff Location",
+                    prefixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)))),
+              ),
+            ),
+          ],
+        ),
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: ElevatedButton(

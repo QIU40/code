406c0c0396f494e506975c00c701e73749e9fc92
QIU40 <106186013+QIU40@users.noreply.github.com>
2022-11-20 04:31:10 +0800

    Update menuui.dart

--- menuui.dart
+++ menuui.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 class MenuPage extends StatefulWidget {
@@ -21,51 +23,47 @@ class _MenuPage extends State<MenuPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: 300,
-        height: 200,
-        child: Column(
-          children: <Widget>[
-            Positioned(
-              top: 110,
-              left: 10,
-              child: TextField(
-                onChanged: (value) {},
-                controller: editingController,
-                decoration: InputDecoration(
-                    labelText: "Pickup",
-                    hintText: "Pickup Location",
-                    prefixIcon: Icon(Icons.search),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)))),
-              ),
-            ),
-            Positioned(
-              top: 50,
-              right: 20,
-              child: TextButton(
-                onPressed: swapValues,
-                child: const Text("swap"),
-              ),
-            ),
-            Positioned(
-              top: 110,
-              left: 10,
-              child: TextField(
-                onChanged: (value) {},
-                controller: editingController2,
-                decoration: InputDecoration(
-                    labelText: "Dropoff",
-                    hintText: "Dropoff Location",
-                    prefixIcon: Icon(Icons.search),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)))),
-              ),
-            ),
-          ],
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Column(children: [
+          TextField(
+            onChanged: (value) {},
+            controller: editingController,
+            decoration: InputDecoration(
+                labelText: "Pickup",
+                hintText: "Pickup Location",
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)))),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            heightFactor: 1.0,
+            child: ElevatedButton(
+                onPressed: swapValues, child: const Text("swap")),
+          ),
+          const SizedBox(height: 15),
+          TextField(
+            onChanged: (value) {},
+            controller: editingController2,
+            decoration: InputDecoration(
+                labelText: "Dropoff",
+                hintText: "Dropoff Location",
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)))),
+          ),
+        ]),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ElevatedButton(
+            onPressed: swapValues,
+            child: const Text("Search"),
+          ),
         ),
-      ),
+      ],
     );
   }
 }

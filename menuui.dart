7ac3264bd155ed73365168d734298433caa755bd
QIU40 <106186013+QIU40@users.noreply.github.com>
2022-12-01 04:23:19 +0800

    Update menuui.dart

--- menuui.dart
+++ menuui.dart
@@ -28,7 +28,8 @@ class _MenuPage extends State<MenuPage> {
         Column(
           children: [
             Container(
-              margin: EdgeInsets.fromLTRB(10, 10, 80, 10), //<, ^, >, v
+              margin: EdgeInsets.fromLTRB(10, 10, 80, 0), //<, ^, >, v
+              height: 50.0,
               child: TextField(
                 onChanged: (value) {},
                 controller: editingController,
@@ -40,16 +41,19 @@ class _MenuPage extends State<MenuPage> {
                         borderRadius: BorderRadius.all(Radius.circular(10.0)))),
               ),
             ),
-            Container(
-              margin: EdgeInsets.fromLTRB(70, 0, 10, 0), //<, ^, >, v
-              child: Align(
-                alignment: Alignment.bottomRight,
-                heightFactor: 1.0,
-                child: ElevatedButton(
-                    onPressed: swapValues, child: const Text("swap")),
-              ),
+            /**/ Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  //margin: EdgeInsets.fromLTRB(70, 0, 10, 0), //<, ^, >, v
+
+                  child: ElevatedButton(
+                      onPressed: swapValues, child: const Text("swap")),
+                ),
+              ],
             ),
             Container(
+              height: 50.0,
               margin: EdgeInsets.fromLTRB(10, 0, 80, 0),
               child: TextField(
                 onChanged: (value) {},
@@ -64,6 +68,8 @@ class _MenuPage extends State<MenuPage> {
             ),
           ],
         ),
+
+        //Data Picker Method Needed soon.
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: ElevatedButton(
@@ -71,6 +77,8 @@ class _MenuPage extends State<MenuPage> {
             child: const Text("Search"),
           ),
         ),
+
+        Container(margin: EdgeInsets.fromLTRB(0, 20, 0, 0), child: Passenger()),
       ],
     );
   }

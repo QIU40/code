35d800e5b05266f6353a7bfc6ccf37099b34bf09
QIU40 <106186013+QIU40@users.noreply.github.com>
2022-12-01 04:32:11 +0800

    Update passenger.dart

--- passenger.dart
+++ passenger.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'passengerdetails.dart';
 
 class Passenger extends StatefulWidget {
   const Passenger({Key? key}) : super(key: key);
@@ -9,47 +10,72 @@ class Passenger extends StatefulWidget {
 
 class _Passenger extends State<Passenger> {
   int passenger = 1;
-  final List<String> name = <String>[
-    'Alex',
-  ];
-  final List<int> age = <int>[22];
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      Column(children: [
-        Table(border: TableBorder.all(), children: [
-          //Passenger()
-          TableRow(children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                // color: Color.fromARGB(255, 48, 255, 252),
-                Text(
-                  "Number of Passengers: ",
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                ),
-                Text(
-                  passenger.toString(),
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                ),
-              ],
-            ),
-          ]),
-          TableRow(children: [Text('Javatpoint')]),
-          TableRow(children: [
-            Container(
-              //margin: EdgeInsets.fromLTRB(70, 0, 10, 0), //<, ^, >, v
+    return Column(
+      children: <Widget>[
+        Column(
+            //Passenger()
+            children: [
+              Table(
+                border: TableBorder.all(),
+                children: [
+                  //Passenger()
+                  TableRow(
+                    children: [
+                      Row(
+                        //Row 1 - Number of Passengers
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            height: 30,
+                            child: const Text(
+                              "Number of Passengers: ",
+                              style: TextStyle(
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            height: 30,
+                            child: Text(
+                              passenger.toString(),
+                              style: const TextStyle(
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
+              Row(
+                  //Row 2 - Passenger Details
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                        child: Container(
+                      height: 250,
+                      color: const Color.fromARGB(255, 195, 244, 244),
+                      child: const Center(child: PassengerDetails()),
+                    ))
+                  ]),
+            ]),
+        Row(
+            //Row 3 - Buttons may vary when touching add button
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                //margin: EdgeInsets.fromLTRB(70, 0, 10, 0), //<, ^, >, v
 
-              child: ElevatedButton(onPressed: () {}, child: const Text("Add")),
-            ),
-          ]),
-        ])
-      ])
-    ]);
+                child:
+                    ElevatedButton(onPressed: () {}, child: const Text("Add")),
+              ),
+            ]),
+      ],
+    );
   }
 }
